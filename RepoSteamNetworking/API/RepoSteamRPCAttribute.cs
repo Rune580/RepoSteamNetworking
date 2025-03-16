be1274bb1720aed0b92152c12581976a9134108d
Rune580 <runemaster580@gmail.com>
2025-03-16 12:53:32 -0400

    starting work on source generator

---
+++ RepoSteamNetworking/API/RepoSteamRPCAttribute.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace RepoSteamNetworking.API;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class RepoSteamRPCAttribute : Attribute
+{
+    
+}
---
+++ RepoSteamNetworking/Testing/ExampleBehaviour.cs
@@ -0,0 +1,13 @@
+using RepoSteamNetworking.API;
+using UnityEngine;
+
+namespace RepoSteamNetworking.Testing;
+
+public partial class ExampleBehaviour : MonoBehaviour
+{
+    [RepoSteamRPC]
+    public void SendDataRPC()
+    {
+        
+    }
+}
